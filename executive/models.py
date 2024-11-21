2d33a3a62fd141b59153696304e41e9a96496abd
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-21 13:26:21 -0600

    updated

--- executive/models.py
+++ executive/models.py
@@ -27,7 +27,7 @@ class Executive(models.Model):
     designation = models.CharField(max_length=50, choices=DESIGNATION_CHOICES)
     photo = models.ImageField(upload_to='executives/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    linkedIn = models.URLField(blank=True, null=True)
+    linkedIn = models.URLField(blank=True, null=True) # Add linkedIn field
 
     def __str__(self):
         return f"{self.name} ({self.designation})"
