16ce162c2d56449b03a06d96011a885066596d85
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-22 04:42:44 -0600

    db change

--- computer_club/settings.py
+++ computer_club/settings.py
@@ -26,14 +26,20 @@ SECRET_KEY = 'django-insecure-3u!(ut4k*k_xg8lbswch_0k38x^!jw2!y+++vjh@%s1ovepgo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = "user.User"
 
+CSRF_TRUSTED_ORIGINS = ['https://natures-paradise-stlb.onrender.com','https://*.127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1:8000',
+    
+]
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
     
     
     'django.contrib.admin',
@@ -89,23 +95,23 @@ WSGI_APPLICATION = 'computer_club.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.twijefxkfygwchllwosd',
-        'PASSWORD': 'fazlerabbi1972',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '6543'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.twijefxkfygwchllwosd',
+#         'PASSWORD': 'fazlerabbi1972',
+#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+#         'PORT': '6543'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
