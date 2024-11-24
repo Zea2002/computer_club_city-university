6d30da3fd23b153d8e789838a894df10e8d429d1
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-24 08:15:15 -0600

    CorsMiddleware added

--- computer_club/settings.py
+++ computer_club/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'computer_club.urls'
