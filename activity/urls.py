9dd0ca786ce2153ca797781237504556f76dff89
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-24 07:32:20 -0600

    updated

--- activity/urls.py
+++ activity/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ActivityListCreateView, ResultListView,ParticipantListView
+from .views import ActivityListCreateView, ResultListView,ParticipantListView,ParticipantRegistrationView
 
 urlpatterns = [
     path('activities/', ActivityListCreateView.as_view(), name='activity-list-create'),
     path('activities/<int:activity_id>/results/', ResultListView.as_view(), name='result-list'),
     path('activities/<int:activity_id>/participants/', ParticipantListView.as_view(), name='participant-list'),
+    path('activities/<int:activity_id>/register/', ParticipantRegistrationView.as_view(), name='participant-register'),
 ]
--- message/models.py
+++ message/models.py
@@ -3,7 +3,13 @@ from django.conf import settings
 
 class Message(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='received_messages',
+        null=True,  # Allows the receiver to be optional for broadcast
+        blank=True
+    )
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
