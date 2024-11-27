b74b3b92955c874411a22e3de326945faa8531a7
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-27 00:17:27 -0600

    updated message

--- message/views.py
+++ message/views.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -9,32 +8,37 @@ from .serializers import MessageSerializer
 class MessageViewSet(ModelViewSet):
     queryset = Message.objects.all().order_by('-timestamp')
     serializer_class = MessageSerializer
-    
 
     def get_queryset(self):
         user = self.request.user
-        # Include both user-specific and broadcast messages
-        return Message.objects.filter(Q(receiver=user) | Q(receiver__isnull=True)).order_by('-timestamp')
+        if user.is_authenticated:
+            # Logged-in user: Show user-specific and broadcast messages
+            return Message.objects.filter(Q(receiver=user) | Q(receiver__isnull=True)).order_by('-timestamp')
+        else:
+            # Anonymous user: Show only broadcast messages
+            return Message.objects.filter(receiver__isnull=True).order_by('-timestamp')
 
     def perform_create(self, serializer):
-        # If no receiver is specified, it becomes a broadcast message
-        serializer.save(sender=self.request.user)
+        if self.request.user.is_authenticated:
+            serializer.save(sender=self.request.user)
+        else:
+            serializer.save(sender=None)  # If sender is anonymous, set to None or a default value
 
     def retrieve(self, request, *args, **kwargs):
-        """Fetch a single message and mark it as read if applicable."""
+        """Fetch a single message."""
         instance = self.get_object()
-        if instance.receiver == request.user and not instance.is_read:
+        if request.user.is_authenticated and instance.receiver == request.user and not instance.is_read:
             instance.is_read = True
             instance.save()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
-        """Fetch all messages for the logged-in user."""
+        """Fetch all messages, with different behavior for logged-in and anonymous users."""
         queryset = self.get_queryset()
-
-        # Automatically mark all user-specific unread messages as read
-        queryset.filter(receiver=request.user, is_read=False).update(is_read=True)
+        if request.user.is_authenticated:
+            # Mark unread messages as read for logged-in users
+            queryset.filter(receiver=request.user, is_read=False).update(is_read=True)
 
         page = self.paginate_queryset(queryset)
         if page is not None:
